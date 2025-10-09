7efd8bb6bae4a91dd6521d5483db7656efa81918
premprakash yadav <84430639+premprakash8080@users.noreply.github.com>
2025-10-09 12:53:23 +0530

    create an api for signup and login

---
+++ cafe_api/routes/user.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const connection = require("../connnection");
+const router = express.Router();
+
+const jwt = require("jsonwebtoken");
+const { config } = require("dotenv");
+const nodemailer = require('nodemailer');
+require("dotenv").config();
+
+router.post("/signup", (req, res) => {
+  let user = req.body;
+  query = "select email,password,role,status from user where email=?";
+  connection.query(query, [user.email], (err, results) => {
+    if (!err) {
+      if (results.length <= 0) {
+        query =
+          "insert into user(name,contactNumber,email,password,status,role) values(?,?,?,?,'false','user')";
+        connection.query(
+          query,
+          [user.name, user.contactNumber, user.email, user.password],
+          (err, results) => {
+            if (!err) {
+              return res
+                .status(200)
+                .json({ message: "Succesfully Registered" });
+            } else {
+              return res.status(500).json(err);
+            }
+          }
+        );
+      } else {
+        return res.status(400).json({ message: "Email Already Exist." });
+      }
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+
+router.post("/login", (req, res) => {
+  const user = req.body;
+  query = "select email,password,role,status from user where email=?";
+  connection.query(query, [user.email], (err, results) => {
+    if (!err) {
+      if (results.length <= 0 || results[0].password != user.password) {
+        return res
+          .status(401)
+          .json({ message: "Incorrect Username or Password" });
+      } else if (results[0].status === "false") {
+        return res.status(401).json({ message: "Wait for Admin Approval" });
+      } else if (results[0].password == user.password) {
+        const response = { email: results[0].email, role: results[0].role };
+        const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, {
+          expiresIn: "8h",
+        });
+        res.status(200).json({ token: accessToken });
+      } else {
+        return res
+          .status(400)
+          .json({ message: "Something Went Wrong.Please try again later" });
+      }
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+
+
+
+
+
+module.exports = router;

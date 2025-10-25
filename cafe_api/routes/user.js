ddd176de02bd75809976593729fce0dc2bfb8daa
PremPrakash <premprakash8080@gmail.com>
2025-10-25 12:55:11 +0530

    Update user.js

--- cafe_api/routes/user.js
+++ cafe_api/routes/user.js
@@ -109,6 +109,38 @@ router.post('/forgotPassword',(req,res)=>{
 
 router.get('/get',(req,res)=>{
   var query ="select id,name,email,contactNumber,status from user where role='user'"
+  connection.query(query,(err,results)=>{
+    if(!err){
+      return res.status(200).json(results);
+    }
+    else{
+      return res.status(500).json(err);
+    }
+  })
 })
 
+router.patch('/update',(res)=>{
+  let user = res.body;
+  var query = "update user set status=? where id=?";
+  connection.query(query,[user.status,user.id],(err,results)=>{
+    if(!err){
+      if(results.affectedRows == 0){
+        return res.status(404).json({message:"User id does not exist"});
+      }
+      return res.status(200).json({message:"User Updated Successfully"});
+    }
+    else{
+      return res.status(500).json(err);
+
+    }
+  })
+})
+
+router.get('/checkToken',(req,res)=>{
+  return res.status(200).json({message:"true"});
+})
+
+router.post('/changePassword',(req,res)=>{
+  
+})
 module.exports = router;
